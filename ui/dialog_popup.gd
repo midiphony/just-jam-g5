52f0e2dc34b6f19fe6ca1ea15ff0224465b78d60
midiphony <midiphony@gmail.com>
2023-12-15 11:49:09 +0100

    Add working dialog_popup

--- ui/dialog_popup.gd
+++ ui/dialog_popup.gd
@@ -1,15 +1,38 @@
 extends Control
 
-var dialog_data : Dialog
+@export var dialog_data : Dialog
+
+@onready var text_label := $Label
+
+var entry_format_string = "> %s : %s\n"
+
+var last_entry_index := -1
+
+func set_up_dialog(dialog : Dialog):
+	dialog_data = dialog
+	last_entry_index = 0
+	
+	text_label.text = ""
+	add_entry(dialog_data.dialog_entries[0])
+
+func add_entry(dialog_entry : DialogEntry):
+	var speaker = dialog_entry.speaker
+	var entry_text = dialog_entry.text
+	
+	text_label.text += entry_format_string % [speaker, entry_text]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if dialog_data != null:
+		set_up_dialog(dialog_data)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	
-	#if Input.is_action_just_pressed("ui_accept"):
-		#visible = false
+	if visible and Input.is_action_just_pressed("ui_accept"):
+		if last_entry_index < dialog_data.dialog_entries.size() - 1:
+			last_entry_index += 1
+			add_entry(dialog_data.dialog_entries[last_entry_index])
+		else:
+			visible = false
